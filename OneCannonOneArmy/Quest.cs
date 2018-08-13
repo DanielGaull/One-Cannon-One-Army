2e246caf6f12ee1601889fa07ba839245d750250
Duoplus Software <danieljgaull@gmail.com>
2018-08-13 18:54:30 -0500

    Beginning work on quests

---
+++ OneCannonOneArmy/Quest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCannonOneArmy
+{
+    public struct Quest
+    {
+        public QuestGoalType GoalType;
+        public int TypeId;
+        public int GoalNumber;
+        public int RewardCoins;
+        public Quest(QuestGoalType goal, int typeId, int goalNum, int rewardCoins)
+        {
+            GoalType = goal;
+            TypeId = typeId;
+            GoalNumber = goalNum;
+            RewardCoins = rewardCoins;
+        }
+    }
+
+    public class QuestInterface
+    {
+        MenuButton questButton;
+    }
+    public class QuestPopup
+    {
+        Quest quest;
+        string questText = "";
+        Vector2 questTextLoc;
+    }
+
+    public enum QuestGoalType
+    {
+        KillAliens,
+        FireProjectiles,
+        CraftProjectiles,
+        BuyMaterials,
+        SpendCoins,
+        ObtainCoins,
+        BeatLevels,
+    }
+}
